b73e70c7b0e89d465df2f828ffebbbd367bd79fd
Javi Cabrera <javai2002@gmail.com>
2024-04-22 00:33:46 +0200

    Prueba tests

--- test_division.py
+++ test_division.py
@@ -5,7 +5,7 @@ from division import dividir
 class TestDividir(unittest.TestCase):
     def test_dividir(self):
         self.assertEqual(dividir(4,2),2)
-        self.assertEqual(dividir(-1,1),1)
+        self.assertEqual(dividir(-1,1),-1)
         self.assertEqual(dividir(-1,-1),1)
         self.assertEqual(dividir(0,2),0)
         self.assertEqual(dividir(2,0),'ZeroDivisionError: division by zero')
