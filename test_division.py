6a681c2d80e68c61862d4373fc18806971196b01
Javi Cabrera <javai2002@gmail.com>
2024-04-22 00:43:45 +0200

    Prueba Tests Video

--- test_division.py
+++ test_division.py
@@ -5,7 +5,7 @@ from division import dividir
 class TestDividir(unittest.TestCase):
     def test_dividir(self):
         self.assertEqual(dividir(4,2),2)
-        self.assertEqual(dividir(-1,1),-1)
+        self.assertEqual(dividir(-2,1),-2)
         self.assertEqual(dividir(-1,-1),1)
         self.assertEqual(dividir(0,2),0)
 
